bb6fc40844a340a7adfe3494505a44cd83c78712
SatendraPal <palsatyendra9984@gmail.com>
2021-10-30 11:47:44 +0530

    Third utility Commit

--- app/src/main/java/com/example/navigationbookmark/ViewModel/BookmarkViewModelFactory.kt
+++ app/src/main/java/com/example/navigationbookmark/ViewModel/BookmarkViewModelFactory.kt
@@ -4,8 +4,8 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class BookmarkViewModelFactory(private val application: Application):ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return BookmarkViewModel(application) as T
-    }
-}
+//class BookmarkViewModelFactory(private val application: Application):ViewModelProvider.Factory {
+//    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+//        return BookmarkViewModel(application) as T
+//    }
+//}
